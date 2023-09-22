49020dd249367ba1258c2bdfb42d3d223fe95902
Fernanda Osorio <A01026502@tec.mx>
2023-09-21 18:20:10 -0600

    plis ya

--- test-admin1/src/MyLayout.tsx
+++ test-admin1/src/MyLayout.tsx
@@ -5,7 +5,7 @@ import { MenuItem } from '@mui/material';
 import ExitIcon from '@mui/icons-material/PowerSettingsNew';
 
 // It's important to pass the ref to allow Material UI to manage the keyboard navigation
-const MyLogoutButton = forwardRef((props, ref) => {
+export const MyLogoutButton = forwardRef((props, ref) => {
     const logout = useLogout();
     const handleClick = () => logout();
     return (
---
+++ test-admin1/src/UTests.test.tsx
@@ -0,0 +1,44 @@
+import {describe, expect, test} from '@jest/globals';
+import {sum} from './sum';
+import {render, fireEvent, screen, waitFor} from '@testing-library/react'
+import {AdminContext , testDataProvider} from "react-admin";
+import MyLayout from './MyLayout';
+import React from 'react';
+import "@testing-library/jest-dom";
+import { BrowserRouter } from "react-router-dom"; 
+import MyLoginPage from './MyLoginPage';
+import  MyLogoutButton from './MyLayout';
+import userEvent from '@testing-library/user-event'
+
+// Mock the useLogout hook
+// jest.mock('react-admin', () => ({
+//   ...jest.requireActual('react-admin'),
+//   useLogout: jest.fn(),
+// }));
+
+// import userEvent from '@testing-library/user-event'
+import { createMemoryHistory } from 'history'
+import '@testing-library/jest-dom';
+
+
+describe('sum module', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
+
+
+
+describe("my layout", () => {
+  test("Logout button", async () => {
+    render(
+      <AdminContext>
+        <MyLogoutButton />
+      </AdminContext>
+    );
+
+  });
+
+});
+
+
