2fe00e3d05bbbe52ed4b4a42db8f4d2bc1455559
Fernanda Osorio <A01026502@tec.mx>
2023-09-21 19:53:33 -0600

    test no sirve

--- test-admin1/src/UTests.test.tsx
+++ test-admin1/src/UTests.test.tsx
@@ -1,44 +1,50 @@
-// import {describe, expect, test} from '@jest/globals';
-// import {sum} from './sum';
-// import {render, fireEvent, screen, waitFor} from '@testing-library/react'
-// import {AdminContext , testDataProvider} from "react-admin";
-// import MyLayout from './MyLayout';
-// import React from 'react';
-// import "@testing-library/jest-dom";
-// import { BrowserRouter } from "react-router-dom"; 
-// import MyLoginPage from './MyLoginPage';
-// import  MyLogoutButton from './MyLayout';
-// // import userEvent from '@testing-library/user-event'
+import {describe, expect, test} from '@jest/globals';
+import {sum} from './sum';
+import {render, fireEvent, screen, waitFor} from '@testing-library/react'
+import {AdminContext , testDataProvider} from "react-admin";
+import MyLayout from './MyLayout';
+import React from 'react';
 
-// // Mock the useLogout hook
-// // jest.mock('react-admin', () => ({
-// //   ...jest.requireActual('react-admin'),
-// //   useLogout: jest.fn(),
-// // }));
+import { BrowserRouter } from "react-router-dom"; 
+import MyLoginPage from './MyLoginPage';
+import  MyLogoutButton from './MyLayout';
+import {MyAppBar } from './MyAppBar';
+import '@testing-library/jest-dom'
+// import userEvent from '@testing-library/user-event'
 
-// // import userEvent from '@testing-library/user-event'
-// // import { createMemoryHistory } from 'history'
-// // import '@testing-library/jest-dom';
+// Mock the useLogout hook
+// jest.mock('react-admin', () => ({
+//   ...jest.requireActual('react-admin'),
+//   useLogout: jest.fn(),
+// }));
 
-
-// describe('sum module', () => {
-//   test('adds 1 + 2 to equal 3', () => {
-//     expect(sum(1, 2)).toBe(3);
-//   });
-// });
+// import userEvent from '@testing-library/user-event'
+// import { createMemoryHistory } from 'history'
+// import '@testing-library/jest-dom';
 
 
+describe('sum module', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
 
-// describe("my layout", () => {
-//   test("Logout button", async () => {
-//     render(
-//       <AdminContext>
-//         <MyLogoutButton />
-//       </AdminContext>
-//     );
 
-//   });
 
-// });
+describe("my app bar si esta plis ya", () => {
+  test("appbar appear ", async () => {
+    const { container } = render(
+        <AdminContext>
+          <MyAppBar />
+      </AdminContext>
+      );
+    
+
+  });
+
+  const div = screen.getByText("MyAppBar");
+  expect(div).toBeInTheDocument();
+
+});
 
 
