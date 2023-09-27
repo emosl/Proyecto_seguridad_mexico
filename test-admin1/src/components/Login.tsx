f378ea28f596f92264e85a3ec2b0a50c1f500c4a
MartinPaGarcia <a01066569@itesm.mx>
2023-09-26 22:41:31 -0600

    Login

---
+++ test-admin1/src/components/Login.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { useState } from 'react';
+import Paper from '@mui/material/Paper';
+import { useLogin, useNotify, Notification } from 'react-admin';
+
+export const Login: (React.FC) = () => {
+    
+};
+
+export default Login;
