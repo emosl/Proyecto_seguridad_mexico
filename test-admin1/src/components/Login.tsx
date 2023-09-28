5651760eceaa6c51ae828b6b8d8ba37dfbb452bb
MartinPaGarcia <a01066569@itesm.mx>
2023-09-27 23:37:25 -0600

    Login start design

--- test-admin1/src/components/Login.tsx
+++ test-admin1/src/components/Login.tsx
@@ -2,8 +2,23 @@ import * as React from 'react';
 import { useState } from 'react';
 import Paper from '@mui/material/Paper';
 import { useLogin, useNotify, Notification } from 'react-admin';
+import  { AuthProvider } from "react-admin";
+import { Avatar, Grid } from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
 
 export const Login: (React.FC) = () => {
+    const paperStyle = {padding: 20, height: '70vh', width: 280, margin: "20px auto"};
+    return(
+        <Grid>
+            <Paper elevation = {10} style = {paperStyle}>
+            <Grid container direction="column" justifyContent="center" alignItems="center">
+                <Avatar><AccountCircleIcon/></Avatar>
+                <h2>Sign In</h2>
+                </Grid>
+            </Paper>
+        </Grid>
+    )
     
 };
 
