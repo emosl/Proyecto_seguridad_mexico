af1a2774f8eb730633c5f71ddd430e02cdf9420b
Emilia Salazar <emiliasalazar67@gmail.com>
2023-10-03 23:09:54 -0600

    crear ticktets, database separado

--- backend/data_base/collections/Tickets.js
+++ backend/data_base/collections/Tickets.js
@@ -1,39 +1,12 @@
-[{
-
-  "id": 4,
-  "usuario": "chamito813",
-  "contrasena": "1234",
-  "nombre": "Samuel",
-  "apellidoPaterno": "Acevedo",
-  "apellidoMaterno": "Sandoval",
-  "rol": "Coordinador"
-},
-{
-
-  "id": 5,
-  "usuario": "chamito813",
-  "contrasena": "1234",
-  "nombre": "Samuel",
-  "apellidoMaterno": "Sandoval",
-  "apellidoPaterno": "Acevedo",
-  "rol": "Administrador"
-},
-{
-  "id": 6,
-  "usuario": "Martin",
-  "contrasena": "1234",
-  "nombre": "Martin",
-  "apellidoMaterno": "Garcia",
-  "apellidoPaterno": "Palomares",
-  "rol": "Ejecutivo"
-},
-{
-  "usuario": "Fer123",
-  "contraseña": "$2b$10$ohZexClISO04dlsHl.wJUOJ/3JLzIofv9h99ls3I5H1MD6.r4RPSe",
-  "nombre": "fernanda osorio"
-},
-{
-  "usuario": "Emo123",
-  "contraseña": "$2b$10$Zd782fAI.ZpdFAEFDcQrFe6YmW1hgjsWXKegFxJNrilMTQapThocy",
-  "nombre": "Emilia Salazar"
-}]
+[
+  {
+    "fechaDeCreacion":"2023-10-04T04:58:50.567Z",
+"clasificacion":"Digital",
+"nivelDePrioridad":"bajo",
+"numeroDeIntermediarios":3,
+"Qué, cómo y si no se resolvió ¿por qué?":"Bien",
+"tipoDeIncidencia":"Hardware",
+"Tiempo que tardó en atenderse (días)":5,
+"id":6
+  }
+]
