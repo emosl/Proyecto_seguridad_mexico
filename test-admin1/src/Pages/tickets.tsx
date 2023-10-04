58132856945f617ee338099aedc843683b0ba1c1
Emilia Salazar <emiliasalazar67@gmail.com>
2023-10-03 20:47:31 -0600

    lol

---
+++ test-admin1/src/Pages/tickets.tsx
@@ -0,0 +1,24 @@
+import {
+    List,
+    Datagrid,
+    TextField,
+    ReferenceField,
+    EditButton,
+    Edit,
+    Create,
+    SimpleForm,
+    ReferenceInput,
+    TextInput,
+} from "react-admin";
+import { useRecordContext} from "react-admin";
+
+
+export const TicketsCreate = () => (
+    <Create>
+      <SimpleForm>
+        <ReferenceInput source="userId" reference="users" />
+        <TextInput source="title" />
+        <TextInput source="body" multiline rows={5} />
+      </SimpleForm>
+    </Create>
+  );
