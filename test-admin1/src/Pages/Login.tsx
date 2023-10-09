04cbd4d674bfb4c7a6c7cb4ac5d1f0541f5f21ae
Emilia Salazar <emiliasalazar67@gmail.com>
2023-10-08 22:48:45 -0600

    login nuevo

--- test-admin1/src/Pages/Login.tsx
+++ test-admin1/src/Pages/Login.tsx
@@ -1,139 +1,75 @@
 import * as React from 'react';
 import { useState } from 'react';
-import Paper, { PaperProps } from '@mui/material/Paper';
-import { useLogin, useNotify, Button, Link } from 'react-admin';
-import { Avatar, FormControlLabel, Grid, TextField, Checkbox, Typography, Box } from '@mui/material';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import "./App.css"
-import backgroundImage from '../assets/654730.jpg';
+import { useLogin, useNotify } from 'react-admin';
+import "./Login.css";
+import "./App.css";
 
-
-export const Login: (React.FC) = () => {
+export const Login: React.FC = () => {
     const login = useLogin();
     const notify = useNotify();
 
-    // Estilo del paper
-    const paperStyle: PaperProps['style'] = {
-        padding: 20,
-        width: 280,
-        margin: '20px auto',
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        borderRadius: '20px',
-        boxShadow: '10px 20px 25px -5px rgba(0,0,0,0.75)',
-        transform: 'translate(-50%, -50%)',
-        backgroundColor: '#170212',
-        
-    };
-    
-    // Estilo del grid
-    const gridStyle = {
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundSize: 'cover', 
-        height: '100vh',
-        width: '100vw',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      };
-    
-    // Estilo del avatar
-    const avatarStyle = { backgroundColor: '#F5494C' };
-
-    // Componentes de email, password y rememberMe
+ 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [rememberMe, setRememberMe] = useState(false);
     
-    
-    // Función para el submit
-const handleSubmit =  (e: React.FormEvent) => {
+   
+    const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-        
-        // Llama a la función login de react-admin
-        login({ username: email, password: password }).then(()=>
-        // navigate('/admin')
-        window.location.href = '/tickets'
-        )
-        .catch(() =>
-            notify('Invalid email or password')
-        );
+       
+        login({ username: email, password: password })
+        .then(() => {
+            window.location.href = '/tickets'; 
+        })
+        .catch(() => {
+            notify('Invalid email or password'); 
+        });
     };
     
-    // Retorna el formulario
-    return (
-        <Grid container style={gridStyle} >
-            <Grid item xs={6}>
-                <Paper elevation={10} style={paperStyle} >
-                    <Grid container direction="column" justifyContent="center" alignItems="center">
-                        <Avatar style={avatarStyle}><AccountCircleIcon /></Avatar>
-                        <h2>Iniciar sesión</h2>
-                        <form onSubmit={handleSubmit}>
-                            <TextField
-                                label="Username"
-                                placeholder="Enter username"
-                                fullWidth
-                                required
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                style = {{
-                                        marginBottom: '10px', 
-                                        borderRadius: '20px', 
-                                        padding: '2px', 
-                                        fontSize: '12px'
-                                    }}
-                                InputProps={{ sx: { borderRadius: 3 } }}
 
-                                
-                            />
-                            <TextField
-                                label="Password"
-                                placeholder="Enter password"
-                                type="password"
-                                fullWidth
-                                required
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                style = {{
-                                        borderRadius: '10px',
-                                        padding: '2px'
-                                     
-                                    }}
-                                InputProps={{ sx: { borderRadius: 3 } }}
-                            />
-                            <FormControlLabel
-                                control={
-                                    <Checkbox
-                                        name="rememberMe"
-                                        color="primary"
-                                        checked={rememberMe}
-                                        onChange={(e) => setRememberMe(e.target.checked)}
-                                    />
-                                }
-                                label="Remember me"
-                            />
-                            
-                            <Button
-                            type="submit"
-                            variant="contained" 
-                            style={{ borderRadius: 10 , color: "#000000", backgroundColor: "#F5494C", marginBottom: '10px',}}
-                            fullWidth
-                            >
-                            <Typography>Iniciar sesión</Typography>
-                            </Button>
-                        </form>
-                        <Typography style={{ marginBottom: '10px' }}>
-                            <Link to={''} >¿Olvidaste tu contraseña?</Link>
-                            
-                        </Typography >
-                        <Typography>
-                            ¿No tienes cuenta? <Link to={''}>Registrarse</Link>
-                        </Typography>
-                    </Grid>
-                </Paper>
-            </Grid>
-        </Grid>
+    return (
+        <body className="body">
+        <div className="wrapper">
+            <span className="bg-animate">
+            </span>
+            <div className="form-box login">
+                <h2>Login</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="input-box">
+                        <input 
+                            type="text" 
+                            required 
+                            value={email} 
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <label>Username</label>
+                    </div>
+                    <div className="input-box">
+                        <input 
+                            type="password" 
+                            required 
+                            value={password} 
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <label>Password</label>
+                    </div>
+                    <button type="submit" className="btn">Login</button>
+                </form>
+            </div>
+            <div>
+            <img className = "logo"
+                    src="https://www.pormexicofundacion.org/images/logo-wide.png" 
+                    alt="Fundación por México Logo"
+                    width="500" 
+                    height="300" 
+                />
+            </div>
+            <div className="info-text login">
+                <h2>Bienvenido</h2>
+                <p>Sistema de Tickets Fundación por México</p>
+            </div>
+        </div>
+        </body>
     );
 };
 
