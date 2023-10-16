6e70b7c73d5f0219d09ce92e30c0576851f4dba8
MartinPaGarcia <a01066569@itesm.mx>
2023-10-16 17:50:55 -0600

    CERTIFICADOS

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ import { MongoClient } from "mongodb";
 import bodyParser from "body-parser";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import https from "https";
+import fs from "fs";
 
 // let db;
 // const app = express(); // create express app, executes functions
@@ -306,6 +308,12 @@ app.delete("/tickets/:id", async (request, response) => {
     response.sendStatus(401);
   }
 });
+
+
+https.createServer({cert: fs.readFileSync("./backend.cer"), key: fs.readFileSync("./backend.key")}, app).listen(3000, ()=>{
+  connectDB();
+  console.log("Servidor escuchando en puerto 8000")
+})
 
 // app.get("/dashboard", async (request, response) => {
 //   try {
